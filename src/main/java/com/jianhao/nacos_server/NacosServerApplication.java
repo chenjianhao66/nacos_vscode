1f92b44eba00ddd468a61a33c7b0691eb43efd77
陈建浩 <1079170090@qq.com>
2021-07-29 13:39:14 +0800

    添加 NacosInjected注解的使用

--- src/main/java/com/jianhao/nacos_server/NacosServerApplication.java
+++ src/main/java/com/jianhao/nacos_server/NacosServerApplication.java
@@ -1,9 +1,24 @@
 package com.jianhao.nacos_server;
 
+import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.spring.context.annotation.EnableNacos;
+// import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+// import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
+
+// import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 开启nacos的配置中心功能，指定nacso服务器地址
+// @EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "127.0.0.1:8848"))
+// @EnableNacosConfig
+
+// 开启nacos的服务发现功能，制定nacos服务器地址
+// @EnableNacosDiscovery(globalProperties = @NacosProperties(serverAddr = "127.0.0.1:8848"))
+// @EnableNacosDiscovery
+@EnableNacos(globalProperties = @NacosProperties(serverAddr = "127.0.0.1:8848"))
 public class NacosServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jianhao/nacos_server/controller/NamingController.java
+++ src/main/java/com/jianhao/nacos_server/controller/NamingController.java
@@ -1,12 +1,17 @@
 package com.jianhao.nacos_server.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +20,14 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class NamingController {
+
+
+    @NacosInjected
+    private NamingService namingService;
+
+
+    @NacosInjected
+    private ConfigService configService;
     
 
     @ApiOperation("增加服务")
@@ -28,6 +41,14 @@ public class NamingController {
         String result = restTemplate.postForObject(url, "", String.class, map);
         return result;
     }
+    
+    @ApiOperation("获取一个服务下的所有实例")
+    @GetMapping("getAllInstancse")
+    public List<Instance> getAllInstancse(String serviceName)throws NacosException{
+         return namingService.getAllInstances(serviceName);
+    }
+
+
 
     @ApiOperation("注册实例")
     @PostMapping("registerInster")
@@ -39,7 +60,19 @@ public class NamingController {
             return "注册失败！";
         }
         return "注册成功～";
-
-
     }
+
+    @ApiOperation("注销实例")
+    @PostMapping("unregisterInster")
+    public String unregisterInster(String serviceName,String ip,int port){
+        try {
+            namingService.deregisterInstance(serviceName, ip, port);
+            
+        } catch (NacosException e) {
+            e.printStackTrace();
+            return "fail";
+        }
+        return "success~";
+    }
+
 }
